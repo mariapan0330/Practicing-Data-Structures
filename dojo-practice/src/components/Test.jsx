0b405e19ec8e40c2824ec49da7d6ad532d3fc6a7
Maria Panagos <maria.pan0330@gmail.com>
2023-05-24 11:52:09 -0500

    started react practice; learned abt previous state

---
+++ dojo-practice/src/components/Test.jsx
@@ -0,0 +1,50 @@
+// build a text with a border
+// ask the user for an input and whatever they input, put it in the box
+// have a button that hides the component
+
+import React, { useState } from 'react'
+
+export default function Test() {
+    const [val, setVal] = useState("This is a box");
+    const [hideBox, setHideBox] = useState(false);
+
+    console.log(hideBox)
+
+    const formHandler = (e) => {
+        // console.log(e.target.value)
+        setVal(e.target.value)
+    }
+
+    const handleHideBox = () => {
+        setHideBox(p => {
+            return !p
+        })
+
+        // if (hideBox == true){
+        //     setHideBox(false)
+        // } else{
+        //     setHideBox(true)
+        // }
+    }
+
+
+    return (
+        <div>
+            <input name='inp' type="text" onChange={formHandler}></input>
+            {
+                hideBox ?
+                <>
+                <div></div>
+                <button onClick={handleHideBox}>Show the box</button>
+                </>
+                :
+                <>
+                <div style={{borderStyle: 'solid'}}>
+                    {val}
+                </div>
+                <button onClick={handleHideBox}>Hide the box</button>
+                </>
+            }
+        </div>
+    )
+}
